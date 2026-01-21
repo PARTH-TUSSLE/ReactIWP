fc3579e6418da883d395adca8fc9b1fa6130dae6
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2026-01-21 13:15:37 +0530

    routing in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
 import { useEffect, useState } from "react"
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
+import Blogs from "./components/Blogs";
+import Contact from "./components/Contact";
+import TermsOfService from "./components/TermsOfService";
+import Landing from "./components/Landing";
+import NotFound from "./components/NotFound";
 
 function App() {
+  
 
-  const[showTimer, setShowTimer] = useState(true);
+//   const[showTimer, setShowTimer] = useState(true);
 
-  useEffect(() => {
-    setInterval(() => {
-      setShowTimer((prev) => !prev )
-    }, 5000);
-  }, [])
+//   useEffect(() => {
+//     setInterval(() => {
+//       setShowTimer((prev) => !prev )
+//     }, 5000);
+//   }, [])
 
-  return <div>{showTimer && <Timer />}</div>;
+//   return <div>{showTimer && <Timer />}</div>;
+// }
+
+// function Timer () {
+
+//   const [count, setCount] = useState(0);
+
+//   function increaseCount() {
+//     console.log(`from inside the clock`) // still logs even after unmount
+//     setCount((prev) => prev + 1); // if we dont use the setCount(prev => prev + 1) then pass the count state variable inside the dependency array
+//   }
+
+//   useEffect(() => {
+//     let clock = setInterval(increaseCount, 1000);
+//     return () => {
+//       clearInterval(clock); // cleanup function 
+//     }
+//   }, []); // effect will run only on mount because the dependency array is empty or else the effect will run whenever the variables inside the dependency array changes
+
+//   return <div>{count} seconds elapsed!</div>;
+
+
+// ROUTING
+
+return (
+  <div>
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element={<Layout/>} >
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
+
+
 }
 
-function Timer () {
-
-  const [count, setCount] = useState(0);
-
-  function increaseCount() {
-    console.log(`from inside the clock`) // still logs even after unmount
-    setCount((prev) => prev + 1); // if we dont use the setCount(prev => prev + 1) then pass the count state variable inside the dependency array
-  }
-
-  useEffect(() => {
-    let clock = setInterval(increaseCount, 1000);
-    return () => {
-      clearInterval(clock); // cleanup function 
-    }
-  }, []); // effect will run only on mount because the dependency array is empty or else the effect will run whenever the variables inside the dependency array changes
-
-  return <div>{count} seconds elapsed!</div>;
+function Layout () {
+  return (
+    <div>
+      <Link to="/">Landing</Link> | <Link to="/blogs">Blogs</Link> |{" "}
+      <Link to="/contact">Contacts</Link> |{" "}
+      <Link to="terms">Terms of Service</Link>
+      <Outlet />
+      Footer
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/TermsOfService.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function TermsOfService() {
+  const navigate = useNavigate();
+
+  // setInterval(() => {
+  //   navigate("/");
+  // }, 5000);
+
+  return <div>Terms of Service</div>;
+}
+
+export default TermsOfService;
